7dd823c78f069a8b2688b1525c66b3d441028919
Peter Luber <prluber@gmail.com>
2020-03-06 20:31:14 -0800

    organized/finished links

--- src/App.js
+++ src/App.js
@@ -7,21 +7,58 @@ import helados3 from "./helados/helados3.wav";
 import helados4 from "./helados/helados4.wav";
 import helados5 from "./helados/helados5.wav";
 
-let scFrame =
+let scSrc =
   "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/4819303&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
-
-let twFrame =
+let twSrc =
   "https://twitframe.com/show?url=https%3A%2F%2Ftwitter.com%2Fjimmyspacev%2Fstatus%2F1224812067200303112";
+let ytSrc = "https://www.youtube.com/embed/sroQRLtiyVU";
+let bcSrc = "https://accel.bigcartel.com/";
+let bccSrc = "https://jimmyvee.bandcamp.com/";
+let igSrc = "https://www.instagram.com/jimmyspacev/";
 
-let ytFrame = "https://www.youtube.com/embed/sroQRLtiyVU";
+let ytStyle = "width:100%;height:100%;";
+let twStyle =
+  "width:100%;height:110%;transform:scale(1.1)translate(0px,-15px);";
+let bcStyle =
+  "width:300%;height:300%;transform:scale(0.38)translate(-745px,-565px);";
 
 class App extends React.Component {
   state = {
-    iframe: ytFrame
+    iframe: ytSrc
   };
 
   currentFrame = () => {
-    return twFrame;
+    return (
+      twSrc +
+      scSrc +
+      ytSrc +
+      bcSrc +
+      igSrc +
+      bccSrc +
+      bcStyle +
+      ytStyle +
+      twStyle
+    );
+  };
+
+  h1 = new Audio(helados1);
+  h2 = new Audio(helados2);
+  h3 = new Audio(helados3);
+  h4 = new Audio(helados4);
+  h5 = new Audio(helados5);
+
+  keypressApp = event => {
+    if (event.key === "j") {
+      this.h1.play();
+    } else if (event.key === "i") {
+      this.h2.play();
+    } else if (event.key === "m") {
+      this.h3.play();
+    } else if (event.key === "m") {
+      this.h4.play();
+    } else if (event.key === "y") {
+      this.h5.play();
+    }
   };
 
   render() {
@@ -54,15 +91,15 @@ class App extends React.Component {
         </div>
         <div id="bioBox">
           <span id="bioSpan">
-            <a href="https://www.twitter.com/jimmyspacev">twitter</a>
+            <a href={twSrc}>twitter</a>
             <br></br>
-            <a href="http://soundcloud.com/cats-kill">soundcloud</a>
+            <a href={ytSrc}>youtube</a>
             <br></br>
-            <a href="https://www.youtube.com/watch?v=sroQRLtiyVU">youtube</a>
+            <a href={bcSrc}>big cartel</a>
             <br></br>
-            shizuka world
+            <a href={bccSrc}>bandcamp</a>
             <br></br>
-            yang metal
+            <a href="https://soundcloud.com/cats-kill">soundcloud</a>
           </span>
         </div>
         <div id="foot">
@@ -76,26 +113,6 @@ class App extends React.Component {
       </div>
     );
   }
-
-  h1 = new Audio(helados1);
-  h2 = new Audio(helados2);
-  h3 = new Audio(helados3);
-  h4 = new Audio(helados4);
-  h5 = new Audio(helados5);
-
-  keypressApp = event => {
-    if (event.key === "j") {
-      this.h1.play();
-    } else if (event.key === "i") {
-      this.h2.play();
-    } else if (event.key === "m") {
-      this.h3.play();
-    } else if (event.key === "m") {
-      this.h4.play();
-    } else if (event.key === "y") {
-      this.h5.play();
-    }
-  };
 }
 
 export default App;
